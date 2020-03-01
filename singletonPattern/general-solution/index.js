389a5497e7333cf1986f2fb2d04c11901b0c8f49
leetmachine <keegan_gladstone@datahouse.com>
2020-03-01 11:32:06 -0800

    walkthrough the problem and solutions with a Singleton

---
+++ singletonPattern/general-solution/index.js
@@ -0,0 +1,14 @@
+const Logger = require('./Logger');
+const Shopper = require('./Shopper');
+const Store = require('./Store');
+
+const logger = new Logger().getInstance();
+logger.log('starting app...');
+
+const alex = new Shopper('alex', 500);
+const ski_shop = new Store('Steep and Deep supplies', [{item: 'Downhill skis', qty: 5, price: 750}, {item: 'Knit Hat', qty: 20, price: 5}]);
+
+logger.log('finished config...');
+
+console.log(`${logger.count} logs total`);
+logger.logs.forEach(log => console.log(`  ${log.message}`));
---
+++ singletonPattern/node-solution/index.js
@@ -0,0 +1,13 @@
+const logger = require('./Logger');
+const Shopper = require('./Shopper');
+const Store = require('./Store');
+
+logger.log('starting app...');
+
+const alex = new Shopper('alex', 500);
+const ski_shop = new Store('Steep and Deep supplies', [{item: 'Downhill skis', qty: 5, price: 750}, {item: 'Knit Hat', qty: 20, price: 5}]);
+
+logger.log('finished config...');
+
+console.log(`${logger.count} logs total`);
+logger.logs.forEach(log => console.log(`  ${log.message}`));
